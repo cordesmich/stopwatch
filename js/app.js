b74f701d48dc469f7ffaa2a102d3f2b14dede89f
Michael Cordes <cordes.mich@gmail.com>
2017-03-24 16:53:30 +0100

    stopwatch with lap function

---
+++ js/app.js
@@ -0,0 +1,41 @@
+var timer = document.getElementById('timer');
+var toggleBtn = document.getElementById('toggle');
+var resetBtn = document.getElementById('reset');
+var newLap = document.getElementById('lap');
+var delLaps = document.getElementById('delLaps');
+
+var watch = new Stopwatch(timer);
+
+toggleBtn.addEventListener("click", function() {
+    if (watch.isOn) {
+        watch.stop();
+        toggleBtn.textContent = "Start";
+    } else {
+        watch.start();
+        toggleBtn.textContent = "Stop";
+    }
+});
+
+resetBtn.addEventListener("click", function() {
+    watch.reset();
+    
+});
+
+newLap.addEventListener("click", function(){
+   
+   if (watch.isOn) {
+    watch.newLap();
+
+    var text = watch.getLastLap();
+    var row = "<tr>" + "<td>" + text + " </td> </tr>";
+
+    document.getElementById("tbLaps").getElementsByTagName("tbody")[0].innerHTML += row;
+   }
+
+} );
+
+delLaps.addEventListener("click", function () {
+
+    document.getElementById("tbLaps").getElementsByTagName("tbody")[0].innerHTML = "<tr><th>Laps:</th></tr>"
+
+});
---
+++ js/stopwatch.js
@@ -0,0 +1,81 @@
+function Stopwatch(elem) {
+    
+    var time = 0; 
+    var interval;
+    var offset;
+    var laps = [];
+
+    function update() {
+        if (this.isOn) {
+            time += delta();
+        }      
+        //console.log(timeFormatter(time));
+        var formTime = timeFormatter(time);
+        elem.textContent = formTime;
+    };
+
+    function delta() {
+        var now = Date.now();
+        var timePassend = now - offset;
+        offset = now;
+        return timePassend;
+    };
+    
+    function timeFormatter(timeInMilliseconds) {
+        var time = new Date(timeInMilliseconds);
+        var minutes = time.getMinutes().toString();
+        var seconds = time.getSeconds().toString();
+        var milliseconds = time.getMilliseconds().toString();
+
+        if (minutes.length < 2) {
+            minutes = '0' + minutes;
+        }
+        if (seconds.length < 2) {
+            seconds = '0' + seconds;
+        }
+        while (milliseconds.length < 3) {
+            milliseconds = '0' + milliseconds;
+        }
+
+        return minutes + ' : ' + seconds + ' . ' + milliseconds; 
+
+    };
+
+    this.isOn = false;
+    
+    this.start = function(){
+        if(!this.isOn){
+            interval = setInterval(update.bind(this), 10);
+            offset= Date.now();
+            this.isOn = true;
+        }
+    };
+
+    this.stop = function() {
+        if(this.isOn){
+            clearInterval(interval);
+            interval = null;
+            this.isOn = false;
+        }
+    };
+    
+    this.reset = function() {
+        if (!this.isOn){
+            time = 0;
+            update();
+        }
+    };
+
+    this.newLap = function() {
+        var lap = timeFormatter(time);
+        laps.push(lap);
+      
+        //console.log(laps)
+
+    };
+
+    this.getLastLap = function() {
+        
+        return laps[laps.length -1];
+    };
+}
